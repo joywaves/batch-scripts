961304361ccb78124807ced6ad3c792f86260939
Peter S. Chaploutskii <peter.chaploutskii@knowledgent.com>
2018-03-01 14:05:44 -0500

    updated to make it work

--- localuser.cmd
+++ localuser.cmd
@@ -1,13 +1,20 @@
-echo off
-set username=%1
-set fullname=%2
-set newnamee=%3
+﻿@Echo off
+Setlocal EnableDelayedExpansion
+
+IF NOT %1=="" ( set username=%1 ) else ( echo No User name   & goto:eof )
+IF NOT %2=="" ( set fullname=%2 ) else ( echo No Full name   & goto:eof )
+IF NOT %3=="" ( set newnamee=%3 ) else ( echo No Computername& goto:eof)
+
 shift
 shift
 shift
+
 net user %username% "" /ADD /fullname:%fullname%
 net localgroup "Administrators" %username% /ADD
 
-WMIC COMPUTERSYSTEM WHERE CAPTION=’%computername%’ RENAME ‘%newnamee%’
+wmic computersystem where caption='%computername%' RENAME '%newnamee%'
+
+echo Press Enter to reboot laptop
+set /p input=
 
 shutdown.exe /r /t 00
